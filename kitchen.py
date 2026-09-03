75f515b02cbd118e9365113cd86f2c29897cc970
unknown <napat.new916@gmail.com>
2026-09-03 12:57:39 +0700

    A6: addition test, red

--- kitchen.py
+++ kitchen.py
@@ -6,8 +6,26 @@ class Quantity:
     def times(self, multiplier):
         return Quantity(self.amount * multiplier, self.unit)
 
+    def plus(self, other):
+        return Sum(self, other)
+
     def __eq__(self, other):
         return self.amount == other.amount and self.unit == other.unit
 
     def __repr__(self):
         return f"Quantity({self.amount}, {self.unit!r})"
+
+
+class Sum:
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+    def reduce(self, unit):
+        amount = self.left.amount + self.right.amount
+        return Quantity(amount, unit)
+
+
+class Converter:
+    def reduce(self, total, unit):
+        return total.reduce(unit)
